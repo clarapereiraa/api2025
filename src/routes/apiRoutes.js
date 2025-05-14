e86781d474ab7f55b44ecffe0d978b50eef0a017
clarapereiraa <clasipereira@gmail.com>
2025-05-14 13:58:41 -0300

    jtm nas rotas da API

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -18,11 +18,11 @@ router.delete('/org/:id_organizador', orgController.deleteOrg);
 
 //rotas eventoController
 router.post('/evento', eventoController.createEvento);
-router.get('/evento', eventoController.getAllEventos);
+router.get('/evento', verifyJWT, eventoController.getAllEventos);
 router.put('/evento', eventoController.updateEvento);
 router.delete('/evento/:id_evento', eventoController.deleteEvento);
 router.get('/evento/data', eventoController.getEventosPorData);
-router.get("/evento/:data", eventoController.getEventosPorData7Dias);
+router.get("/evento/:data",verifyJWT, eventoController.getEventosPorData7Dias);
 
 //rotas IngressoController
 router.post('/ingresso', ingressoController.createIngresso);
