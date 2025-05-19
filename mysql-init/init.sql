ca192a4cf82aa6b60f7d0ca2c0cefc8c0dad4c24
clarapereiraa <clasipereira@gmail.com>
2025-05-19 16:05:27 -0300

    docker

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `vio_clara` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `vio_clara`;
+
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: vio_clara
